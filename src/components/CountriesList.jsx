eda0ade5c2305d6e875f6dfa8e6de5bc1352894e
Sarvesh Nathe <sarveshnathe1377@gmail.com>
2024-10-31 19:45:01 +0530

    Improved Data Fetching

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,29 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import CountryCard from './CountryCard'
-import CountriesListShimmer from './CountriesListShimmer'
+import React, { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
+import CountriesListShimmer from "./CountriesListShimmer";
 
 export default function CountriesList({ query }) {
-  const [countriesData, setCountriesData] = useState([])
-
+  const [countriesData, setCountriesData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((res) => res.json())
-      .then((data) => {
-        setCountriesData(data)
-      })
-  }, [])
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("Failed To Fetch countries data!");
+        }
+        const data = await response.json();
+        setCountriesData(data);
+      } catch (e) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCountries();
+  }, []);
 
-  if (!countriesData.length) {
-    return <CountriesListShimmer />
+  if (loading) {
+    return <CountriesListShimmer />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
 
   return (
     <>
       <div className="countries-container">
         {countriesData
-          .filter((country) =>
-            country.name.common.toLowerCase().includes(query) || country.region.toLowerCase().includes(query)
+          .filter(
+            (country) =>
+              country.name.common.toLowerCase().includes(query) ||
+              country.region.toLowerCase().includes(query)
           )
           .map((country) => {
             return (
@@ -36,9 +53,9 @@ export default function CountriesList({ query }) {
                 capital={country.capital?.[0]}
                 data={country}
               />
-            )
+            );
           })}
       </div>
     </>
-  )
+  );
 }
